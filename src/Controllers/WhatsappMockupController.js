2e12aacc7e43c2f0e9e1385303b040b4bdf03fe9
rdturkowicz <rdturkowicz@provincianet.com.ar>
2024-01-25 15:44:48 -0300

    prueba 3 saltos de linea %0A

--- src/Controllers/WhatsappMockupController.js
+++ src/Controllers/WhatsappMockupController.js
@@ -87,9 +87,9 @@ exports.nodes = async(req,res,next) => {
                     // "nodo": 5,
                     // "tipo":"carpeta", 
                     // "textoAdicional": "*Conoce la version ideal para vos* \\n \\n *Impulso:*$70.0000 \\n *Crecimiento:*$180.0000 \\n *Evolución:*$330.0000 \\n *Logros:*$500.0000 \\n \\n  *Ingreso mínimo mensual*",
-                    "nodo": 5,
-                    "tipo":"carpeta", 
-                    "textoAdicional": "*Conoce la version ideal para vos*"&"%0A"&"*Impulso:*$70.0000"&"%0A"&"*Crecimiento:*$180.0000"&"%0A"&"Evolución:*$330.0000"&"%0A"&"*Logros:*$500.0000"&"%0A"&"*Ingreso mínimo mensual*",
+                        "nodo": 5,
+                         "tipo": "carpeta",
+                         "textoAdicional": "*Conoce la version ideal para vos*%0A%0A*Impulso:*$70.0000%0A*Crecimiento:*$180.0000%0A*Evolución:*$330.0000%0A*Logros:*$500.0000%0A%0A*Ingreso mínimo mensual*",
                     "subNodo1": {
                         "nodo":9, 
                         "tipo":"archivo", 
